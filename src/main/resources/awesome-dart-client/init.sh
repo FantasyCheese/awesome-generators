c369fe46a609d446122f22e4a2dec0afc25e25ae
Joshua <lincoln.schweitzer.sunyatsen@gmail.com>
2023-01-31 13:01:33 +0800

    fix: dart build_runner delete conflicting outputs

--- src/main/resources/awesome-dart-client/init.sh
+++ src/main/resources/awesome-dart-client/init.sh
@@ -1,5 +1,5 @@
 find . -name "*200_response*" -delete
 grep -v "200_response" lib/model/_index.dart > temp && mv temp lib/model/_index.dart
 flutter pub get
-flutter pub run build_runner build
+flutter pub run build_runner build --delete-conflicting-outputs
 flutter format .
