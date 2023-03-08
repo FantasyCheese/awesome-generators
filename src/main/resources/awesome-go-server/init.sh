48b1b8b85d0330554ea1238b6c931827aa437595
Joshua <lincoln.schweitzer.sunyatsen@gmail.com>
2023-03-08 15:47:39 +0800

    fix: Go init script

--- src/main/resources/awesome-go-server/init.sh
+++ src/main/resources/awesome-go-server/init.sh
@@ -1,2 +1,3 @@
 find . -name "*200_response*" -delete
-docker run --volume "$PWD":/usr/src/app -w /usr/src/app golang:1.20 go mod download && go fmt ./api
+docker run --volume "$PWD":/usr/src/app -w /usr/src/app golang:1.20 go mod download
+docker run --volume "$PWD":/usr/src/app -w /usr/src/app golang:1.20 go fmt ./api
